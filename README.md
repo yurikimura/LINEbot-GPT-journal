82f24c0d637b133a73d0c6a15a7d272c556655af
yurikimura <yuri.76135@gmail.com>
2023-04-19 19:52:08 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+## My Expressive Writing LINE bot
+
+### 🌟Star Me If You Like It!🌟 
+
+.env
+
+```TOKEN``` ... LINE bot TOKEN.  
+```SECRET``` ... LINE bot Channel Secret.  
+```NOTION_TOKEN``` ... NOTION Integration API Token.  
+```NOTION_DATABASE_ID``` ... NOTION Database ID.  
+```GPT_SERVER_URL``` ... The URL for GPT Server. To Establish GPT Server, see https://github.com/yurikimura/ChatGPT-json  
+
+
+to execute:
+
+```
+python app.py
+```
---
+++ app.py
@@ -0,0 +1,76 @@
+
+from datetime import datetime, timedelta
+import json
+import os
+
+from flask import Flask, abort, request
+import requests
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+app = Flask(__name__)
+
+line_token = os.environ.get("TOKEN")
+line_secret = os.environ.get("SECRET")
+notion_token = os.environ.get("NOTION_TOKEN")
+notion_database_id = os.environ.get("NOTION_DATABASE_ID")
+gpt_seerver_url = os.environ.get("GPT_SERVER_URL")
+
+line_bot_api = LineBotApi(line_token)
+handler = WebhookHandler(line_secret)
+
+@app.route("/callback", methods=("GET", "POST"))
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    jp_time = datetime.now() + timedelta(hours=9)
+
+    notion_headers = {"Authorization": f"Bearer {notion_token}",
+            "Content-Type": "application/json","Notion-Version": "2021-05-13"}
+    notion_body = {"parent": { "database_id": notion_database_id},
+        "properties": {
+            "Name": {"title": [{"text": {"content": event.message.text}}]},
+            "Created": {"date": {"start": jp_time.isoformat()}}
+        }}
+    requests.request('POST', url='https://api.notion.com/v1/pages',\
+        headers=notion_headers, data=json.dumps(notion_body))
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        [TextSendMessage(text=chat_reply(event.message.text))]
+    )
+
+def chat_reply(text):
+    gpt_headers = {
+        "Content-Type": "application/json",
+    }
+    gpt_data = f'{{"content":"{text}"}}'
+
+    gpt_response = requests.post(gpt_seerver_url, headers=gpt_headers, data=gpt_data.encode("utf-8"))
+    gpt_result = gpt_response.json()
+    return gpt_result["reply"]
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
